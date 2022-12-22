f55800f44b4ba49440c3aaf69c1ebc7d1e2a061b
FilippoToffoletto <filippotoffoletto@hotmail.it>
2022-12-22 17:58:42 +0100

    [add]find us bar

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -26,9 +26,27 @@
                     </div>
                     <h4>Action Comics</h4>
                 </div>
+                <div class="card">
+                    <div class="img">
+                        <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX"  alt="Action Comics">
+                    </div>
+                    <h4>Action Comics</h4>
+                </div>
+                <div class="card">
+                    <div class="img">
+                        <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX"  alt="Action Comics">
+                    </div>
+                    <h4>Action Comics</h4>
+                </div>
+                <div class="card">
+                    <div class="img">
+                        <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX"  alt="Action Comics">
+                    </div>
+                    <h4>Action Comics</h4>
+                </div>
 
             </div>
-            <div class="btn"><h4>load more</h4></div>
-          </div>
+        </div>
+        <div class="btn"><h4>load more</h4></div>
     </main>
 @endsection
--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -16,6 +16,7 @@
     <!-- vengono iniettati tutti i contenuti delle pagine -->
     @yield('content')
 
+    @include('partials.findUs')
     @include('partials.footer')
 
 
